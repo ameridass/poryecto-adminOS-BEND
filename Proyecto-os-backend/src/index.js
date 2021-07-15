9ee52292c556a9c257db3d4f5ee979f551bef31e
Álvaro Mérida Sosa <asosa@GRUPOFIP.local>
2021-07-14 20:20:07 -0600

    HTTP request para Post signup

--- Proyecto-os-backend/src/index.js
+++ Proyecto-os-backend/src/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const app = express();
-const mySql =  require('mysql')
-const conn =  require('express-myconnection')
+const mySql = require('mysql')
+const conn = require('express-myconnection')
 const routes = require('./routes/rutas')
 
 
 //config 
 app.set('port', process.env.PORT || 8080);
 const dbOptions = {
-    host:"174.138.41.43",
+    host: "174.138.41.43",
     port: 3306,
-    user:'root',
-    password:'#Tn12021T',
-    database:'tn1',
+    user: 'nodejs1',
+    password: '#Tn12021T',
+    database: 'tn1',
     //multipleStatements:true
-    insecureAuth : true
+    insecureAuth: true
 }
 
 //gestion o middleware
-//app.use(conn(mySql, dbOptions, 'single'))
+app.use(conn(mySql, dbOptions, 'single'))
+app.use(express.json())
 
 //rutas
-app.get('/',(req, res) =>{
+app.get('/', (req, res) => {
     res.send('Consumiendo')
 })
 
@@ -30,5 +31,5 @@ app.use('/login', routes)
 
 //configuracion del server
 app.listen(app.get('port'), () => {
-console.log('Escuchando en el puerto '+ app.get('port') +' correctamente')
+    console.log('Escuchando en el puerto ' + app.get('port') + ' correctamente')
 });
--- Proyecto-os-backend/src/routes/rutas.js
+++ Proyecto-os-backend/src/routes/rutas.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 
 
-router.get('/', (req,res) =>{
-  res.send('login funcionando')
+router.get('/', (req, res) => {
+    res.send('login funcionando')
+})
+
+router.post('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        console.log(req.body);
+    })
 })
 
 module.exports = router
