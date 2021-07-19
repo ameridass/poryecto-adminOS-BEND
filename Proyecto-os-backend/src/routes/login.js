abd71a545cbb2353af802453e59417fe3d94ab4e
ameridass <ameridas1998@gmail.com>
2021-07-19 01:54:45 -0600

    Validando loggin

--- Proyecto-os-backend/src/routes/login.js
+++ Proyecto-os-backend/src/routes/login.js
@@ -1,9 +1,9 @@
 const express = require("express");
+//const connection = require("express-myconnection");
 const router = express.Router();
+const conn = require("mysql");
 
-
-
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     req.getConnection((err, conn)=> {
         if(err) return res.send(err)
 
@@ -13,6 +13,29 @@ router.get('/', (req, res) => {
             res.json(rows)
         })
     })
-})
+})*/
+
+router.post("/", (req, res) => {
+  const user = req.body.user;
+  const password = req.body.pass;
+  req.getConnection((err, conn) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      conn.query("SELECT * FROM tn1.users WHERE username = ?", [user], (err, rows) => {
+        if (err) {
+          return res.send(err);
+        } else {
+          if (rows.length == 0) {
+            res.send("Usuario o contrasena no validos");
+            //res.send(rows)
+          } else {
+            res.send("logged in");
+          }
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
